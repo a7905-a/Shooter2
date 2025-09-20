b44711b9b5301b848324f4ee28794bf70122c34e
lansolo7905 <fkshtm43@gmail.com>
2025-09-20 13:27:38 +0900

    애니메이션 추가

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,7 +7,8 @@ public class Weapon : MonoBehaviour
     
     [SerializeField] AimZoom aimZoom;
     [SerializeField] int damageAmount = 1;
-    [SerializeField] ParticleSystem gunFlash;
+    [SerializeField] ParticleSystem gunFlashEffect;
+    [SerializeField] ParticleSystem hitEffect;
     Inputs input;
     Animator animator;
     
@@ -59,11 +60,12 @@ public class Weapon : MonoBehaviour
     void HandleShoot()
     {
         if (!input.shoot) return;
-        gunFlash.Play();
+        gunFlashEffect.Play();
         input.shoot = false;
         RaycastHit rayhit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out rayhit, Mathf.Infinity))
         {
+            Instantiate(hitEffect, rayhit.point, Quaternion.identity); // 오브젝트 풀링으로 변경하고 싶긴함
             EnemyHealth enemyHealth = rayhit.collider.GetComponent<EnemyHealth>();
             if (enemyHealth)
             {
